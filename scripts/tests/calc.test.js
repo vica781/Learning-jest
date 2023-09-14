a337bcb8901cf58e4f355437064e488ce6898311
Victoria Kalenik <vk781@hotmail.com>
2023-09-14 21:00:47 +0000

    Initial commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,21 @@
+const {
+    TestScheduler
+} = require("jest");
+const addition = require("../calc.js");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        })
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
